cf3a3b80cd381143a9732f9c6a8cbd5db7a61115
shangdi1122 <1399070052@qq.com>
2017-08-05 22:12:44 +0800

    Initial commit

---
+++ src/main/java/cn/bdqn/tangcco/tcmp056/ChineseHelloWorld.java
@@ -0,0 +1,13 @@
+package cn.bdqn.tangcco.tcmp056;
+
+import org.junit.Test;
+
+/**
+ * Created by THINKPAD on 2017/8/5.
+ */
+public class ChineseHelloWorld {
+    @Test
+    public void HelloWorld() {
+        System.out.println("hello,world");
+    }
+}
---
+++ src/main/java/cn/bdqn/tangcco/tcmp056/SayHelloWorld.java
@@ -0,0 +1,14 @@
+package cn.bdqn.tangcco.tcmp056;
+
+/**
+ * Created by THINKPAD on 2017/8/5.
+ */
+public class SayHelloWorld {
+    public static void main(String[] args) {
+        System.out.println("hello,world!");
+        System.out.println("hello,world!");
+        System.out.println("你好，世界!");
+
+
+    }
+}
